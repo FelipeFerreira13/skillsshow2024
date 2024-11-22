05b64db8724fa246d867fe5641adf3b6ac5e3721
FelipeFerreira13 <felipeferreira1310@hotmail.com>
2024-11-22 14:46:27 +0000

    general updates

--- src/core/src/main.cpp
+++ src/core/src/main.cpp
@@ -18,39 +18,36 @@ int main(int argc, char **argv)
 
     ROS_INFO("main node is now started");
 
-    move_goal_c    = nh.serviceClient<base_controller::move_goal> ("base_controller/goal" );
-    set_position_c = nh.serviceClient<odometry::pose_odom>        ("odometry/set_position");
-    set_height_c   = nh.serviceClient<oms::set_height>            ("oms/set_height"       );
-    reset_height_c = nh.serviceClient<oms::reset>                 ("oms/reset"            );
-    set_gripper_c  = nh.serviceClient<oms::set_gripper>           ("oms/set_gripper"      );
+    move_goal_c    = nh.serviceClient<base_controller::move_goal> ("base_controller/simple_move/goal" );
+    set_position_c = nh.serviceClient<odometry::pose_odom>        ("odometry/set_position"            );
+    set_height_c   = nh.serviceClient<oms::set_height>            ("oms/set_height"                   );
+    reset_height_c = nh.serviceClient<oms::reset>                 ("oms/reset"                        );
+    set_gripper_c  = nh.serviceClient<oms::set_gripper>           ("oms/set_gripper"                  );
 
     ros::Subscriber start_sub = nh.subscribe("/robot/digital_in/start_button/state", 1, startCallback);
 
-
-    ros::Duration(10).sleep();
-
+    ros::Duration(5).sleep();
 
     do{ 
         ros::spinOnce();
         ros::Duration(0.5).sleep();
+        ROS_INFO("Waiting for Start Button");
     }while ( start_button );
 
 
     // Main Logic
 
-    // reset_height( -1 );
+    oms_driver( -1 );
 
-    // oms_driver( 30 );
+    set_gripper( GRIPPER_OPEN );
 
-    // set_gripper( GRIPPER_OPEN );
+    set_position( 0.3, 0.3, 90 );  
 
-    // set_position( 0.3, 0.3, 90 );  
+    position_driver( 0.3, 1.0, 90 );
 
-    // position_driver( 0.3, 1.0, 90 );
+    oms_driver( 1 );
 
-    // oms_driver( 20 );
-
-    // set_gripper( GRIPPER_CLOSE );
+    set_gripper( GRIPPER_CLOSE );
 
 
     ros::shutdown();
